1333b17ad523f13f0fe31ca312cb24ad479d367e
Rodrigo H. Cordeiro <97626874+rodrigohenriquecordeiro@users.noreply.github.com>
2025-05-27 21:07:44 -0300

    Corrigido dois erros de compilação no Angular

--- BibliotecaRHC.WebApp/src/app/app.config.ts
+++ BibliotecaRHC.WebApp/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 
@@ -13,25 +20,28 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
 
     provideHttpClient(
+      withFetch(), 
       withInterceptors([
         (req, next) => {
-           const token = localStorage.getItem('token');
-           if (token) {
-             const clonedRequest = req.clone({
-               setHeaders: {
-                 Authorization: `Bearer ${token}`
-               }
-             });
-             
-             return next(clonedRequest);
-           }
-           
-           return next(req);
-        }
+          const token =
+            typeof window !== 'undefined'
+              ? localStorage.getItem('token')
+              : null;
+
+          if (token) {
+            const clonedRequest = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+            return next(clonedRequest);
+          }
+
+          return next(req);
+        },
       ])
     ),
-    
     AuthService,
-    AuthGuard
-  ]
+    AuthGuard,
+  ],
 };
